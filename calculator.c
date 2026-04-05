7e19a805c76bb01d0e05042aa2077ae35b2431d2
ConstintCreations <ethanglenn1208@gmail.com>
2026-04-05 11:04:21 -0500

    Basic Calculator

--- calculator.c
+++ calculator.c
@@ -2,7 +2,37 @@
 #include <stdlib.h>
 
 int main() {
-        
+    double a, b, answer;
+    char operator;
+    char *error = NULL;
+
+    printf("First Number: ");
+    scanf("%lf", &a);
+
+    printf("Operator: ");
+    scanf(" %c", &operator);
+
+    printf("Second Number: ");
+    scanf("%lf", &b);
+
+    printf("\n");
+
+    if (operator == '+') {
+        answer = a + b;
+    } else if (operator == '-') {
+        answer = a - b;
+    } else if (operator == '*') {
+        answer = a * b;
+    } else if (operator == '/') {
+        answer = a / b;
+    } else {
+        error = "Unknown Operator. Use '+', '-', '*', or '/'\n";
+    }
+    if (error) {
+        printf(error);
+    } else {
+        printf("%lf %c %lf = %lf\n", a, operator, b, answer);
+    }
 
     return 0;
 }
